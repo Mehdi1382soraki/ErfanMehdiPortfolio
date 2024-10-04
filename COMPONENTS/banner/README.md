a5d8fbf7a39461f9e060d2b5b9c6c5e5e1141b10
Mehdi Soraki <mehdisoraki1382@gmail.com>
2024-10-05 02:21:35 +0330

    Fulfilled some problems of the previous version

--- COMPONENTS/banner/README.md
+++ COMPONENTS/banner/README.md
@@ -1,16 +1,28 @@
-# banner
+# 🎋 banner 
 
 ## ***=> it does not have special width <br />***
 
 <!-- <br /> -->
 
-## ***=> you have to size it by writing the tag in the division <br />***
+## ***=> you have to size it by writing the tag in the container division <br />***
+
+<br />
+
+<!-- <site-banner class="topics__banner" image-address="IMG/Header/Vector.png" header="Peace of Mind" paragraph="the quick fox jumps over the lazy dog"></site-banner> -->
+
+***Tag Name : site-banner*** <br /><br />
+Attributes : >> <br />
+>Class Name : 'topics__banner' || 'topics__banner blue' <br />
+image-address : 'Your iamge url' <br />
+header : 'Sth as header' <br />
+paragraph : 'Sth as explanation' <br />
+
 
 <br />
 
 ***I give you the recommended way to write the style codes for the container :***
 
-max-width: 1046px; <br>
+>max-width: 1046px; <br>
 margin: 0 auto; <br>
 display: flex;<br>
 flex-wrap: wrap;<br>
@@ -18,3 +30,5 @@ justify-content: space-between;<br>
 margin-top: -80px;<br>
 padding: 0 10px;<br>
 gap: 10px;<br>
+
+***Author : Mehdi Soraki*** 🙃
--- JS/banner.js
+++ JS/banner.js
@@ -1,29 +1,50 @@
 import { banner } from "../COMPONENTS/banner/banner-class.js"
 
-let navMenu = document.querySelector('.header__brand-name-div')
-let topics = document.querySelector('.topics')
-let headerOptions = document.querySelector('.header__options')
-let header = document.querySelector('header')
-let modalToTop = document.querySelector('.modalToTop')
 let headerBrandName = document.querySelector('.header__brand-name')
+let navMenu = document.querySelector('.header__brand-name-div')
+let headerOptions = document.querySelector('.header__options')
+let modalToTop = document.querySelector('.modalToTop')
+let topics = document.querySelector('.topics')
+let header = document.querySelector('header')
 
-modalToTop.addEventListener('click', () => {
-    document.documentElement.scrollTop = 0
+/* ====== When you scroll the modalToTop will be appered at the bottom of the page ====== */
+
+window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop >= 91) {
+        modalToTop.style.display = 'flex'
+        modalToTop.style.top = `${document.documentElement.scrollTop + this.window.innerHeight - 70}px`
+    }
 })
 
-headerBrandName.addEventListener('click', () => {
-    location.assign('https://rockpaperscissors.ir')
-})
+/* ====== When the site loaded these two functions will be executed ====== */
 
 window.addEventListener('load', () => {
     setMenuSize()
     changeBannersPlace()
 })
 
+/* ====== When the site resized these two functions will be executed ====== */
+
 window.addEventListener('resize', (e) => {
     setMenuSize()
     changeBannersPlace()
 })
+
+/* ====== When the modal clicked the site scrolls to the top ====== */
+
+modalToTop.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0
+})
+
+/* ====== When the brand name is clicked the site will be refreshed ====== */
+
+headerBrandName.addEventListener('click', () => {
+    location.assign('https://rockpaperscissors.ir')
+})
+
+/**
+ * this void function will decide to show the banners in the header option or in a specific division
+ */
 
 function changeBannersPlace () {
     if (window.innerWidth <= 767 && headerOptions.lastElementChild.className != 'topics') {
@@ -33,6 +54,10 @@ function changeBannersPlace () {
     }
 }
 
+/**
+ * this void function will give the nav bar menu specific style when the size of the site will get less than 767 px
+ */
+
 function setMenuSize () {
     if (window.innerWidth <= 767) {
         navMenu.style.width = `${window.innerWidth-80}px`
@@ -40,10 +65,3 @@ function setMenuSize () {
         navMenu.style.width = `fit-content`
     }
 }
-
-window.addEventListener('scroll', function () {
-    if (document.documentElement.scrollTop >= 91) {
-        modalToTop.style.display = 'flex'
-        modalToTop.style.top = `${document.documentElement.scrollTop + this.window.innerHeight - 70}px`
-    }
-})
